f068d7e8800fa52bbf83cbbef3a32e9226b8006d
AsmaSalah1 <146439635+AsmaSalah1@users.noreply.github.com>
2024-08-04 16:43:58 +0300

    Add files via upload

---
+++ contex/ApplecationDBContex.cs
@@ -0,0 +1,20 @@
+﻿using EF_task1.models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_task1.contex
+{
+    internal class ApplecationDBContex:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-43Q1A7I\\MSSQLSERVER01;Database=EF_Task1;Trusted_Connection=True;TrustServerCertificate=True");
+
+        }
+        public DbSet<product> products { get; set; }
+    }
+}
